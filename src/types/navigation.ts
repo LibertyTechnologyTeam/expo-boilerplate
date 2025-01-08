2d740d2c7b160f36608b117cf6df5e3ec8e5a99a
Bảo Hà <baronn.ha@gmail.com>
2025-01-08 10:27:23 +0700

    ✨ feat: Update app configuration and dependencies; add navigation types. Removed Babel and Metro config files. Enhanced app.json with new plugins and build properties for Android and iOS. Introduced navigation type definitions in src/types/navigation.ts.

---
+++ src/types/navigation.ts
@@ -0,0 +1,30 @@
+import {NavigationProp, RouteProp} from '@react-navigation/native'
+import {NativeStackScreenProps} from '@react-navigation/native-stack'
+
+export type ScreenProps<T extends keyof StackList> = NativeStackScreenProps<StackList, T>
+
+export type RootStackScreenProps<T extends keyof StackList> = NativeStackScreenProps<StackList, T>
+
+export type StackNavigation = NativeStackScreenProps<StackList>['navigation']
+
+export type StackRoute = NativeStackScreenProps<StackList>['route']
+
+export type NavigationType<T extends keyof StackList> = NavigationProp<StackList, T>
+
+export type RouteType<T extends keyof StackList> = RouteProp<StackList, T>
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace ReactNavigation {
+    interface RootParamList extends StackList {}
+  }
+}
+
+export type MainTabParamsType = {
+  icon: string
+  title: string
+}
+
+export type MainTabParamListType = {}
+
+export type StackList = {}
